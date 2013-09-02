4a05f651cf877970173a853ba925b1dd89574d3d
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-09-02 17:20:18 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jstays::Application.config.secret_key_base = '7dc87d4e8dc7f35b7b2e2086e5cf2862e70c28821d713ca0cbaf200ea49c12c9e04437fb59c5d4343b97662fb8766f0186e2318718d7c4276abdf18f44571bd0'
