34aacd0c2b4f30c2bbdcc628482248066e1a1211
Naftali <nafmarcus@gmail.com>
2013-11-03 18:54:52 -0500

    more1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
 	  # Returns the full title on a per-page basis.
+  def admin_user?
+  	signed_in? and current_user.admin
+  end
   def full_title(page_title)
     base_title = "Jstays"
     if page_title.empty?
