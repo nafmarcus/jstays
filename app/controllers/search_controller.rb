28e37ea6a40070a46d46947a2b92178356f7f8f8
Naftali Marcus <naftali@itcentralstation.com>
2014-02-23 15:54:55 -0500

    fix fp search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,9 @@ class SearchController < ApplicationController
 
   def search
 
+    params[:duration] = :long_term if params.keys.join('--').include?('long_term')
+    params[:duration] = :short_term if params.keys.join('--').include?('short_term')
+
     # Default 6 months from today
     @from_date = params[:from_date] ? Date.new(params[:from_date][:year].to_i, params[:from_date][:month].to_i, params[:from_date][:day].to_i) : Date.today
     @to_date = params[:to_date] ? Date.new(params[:to_date][:year].to_i, params[:to_date][:month].to_i, params[:to_date][:day].to_i) : Date.today + 12.month
