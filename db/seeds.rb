a5f0786890b47bea0f51765d30c2b2a67fd32a5d
Rob Jewell <jewell.robertp@gmail.com>
2014-03-26 17:00:17 -0400

    seeds database with properties and begins work on flash message

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 User.destroy_all
 alice = User.create(email: "alice@example.com", first_name: "Alice", last_name: "Smith", password: "password")
 bob = User.create(email: "bob@example.com", first_name: "Bob", last_name: "Smith", password: "password")
+
+Property.destroy_all
+two_bedroom = Property.create(property_type: "Two Bedrooms", title: "Greate Place", sharing: "yes", duration: "5 days", country: "USA", 
+  city: "New York", address: "street", bedrooms: 2.0, sleeps: 2, zip: 12345)
